881f08478c9384f40cf0ec2a1fd9fd9c5b4e1317
kadirrmerci <kadirrmerci@gmail.com>
2026-06-08 01:07:18 +0300

    Rename Android application id to Finvia

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,7 @@
 
 
 android {
-    namespace = "com.elitelife.app"
+    namespace = "com.finvia.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.elitelife.app"
+        applicationId = "com.finvia.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
